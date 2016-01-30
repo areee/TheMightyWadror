db57f9fca4e9767a7f4092880930ac41f69d5bfd
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-01-30 19:09:17 +0200

    exercise 14 done

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts self.name
@@ -11,4 +12,15 @@ class Brewery < ActiveRecord::Base
     self.year = 2016
     puts "changed year to #{year}"
   end
+
+  def average_rating
+
+    sum = 0
+    self.ratings.map { |r| sum+=r.score}
+    count = self.ratings.count
+    average = sum/count.to_f
+
+    "Has #{count} #{"rating".pluralize(count)}, average #{average}"
+  end
+
 end
