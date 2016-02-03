08736205fae849db055bd35c3bdb6751d7b95560
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-03 16:41:41 +0200

    week 3: added user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   # post 'ratings', to: 'ratings#create'
 
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
