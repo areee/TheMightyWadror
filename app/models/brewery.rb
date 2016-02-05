e85f426cd4d6d9be5a6f1d741328f1237fc4687f
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-05 22:03:39 +0200

    tried & skipped exercise 6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,6 +5,7 @@ class Brewery < ActiveRecord::Base
   validates :year, numericality: { greater_than_or_equal_to: 1042,
                                    less_than_or_equal_to: 2016,
                                    only_integer: true }
+  # validate :year_cannot_be_greater_than_this_year
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
@@ -19,5 +20,14 @@ class Brewery < ActiveRecord::Base
     self.year = 2016
     puts "changed year to #{year}"
   end
+
+  # def year_cannot_be_greater_than_this_year
+  #   if Date.today.year > Time.now.year
+  #     reload!
+  #   end
+  #   validates :year, numericality: { greater_than_or_equal_to: 1042,
+  #                                    less_than_or_equal_to: Time.now.year,
+  #                                    only_integer: true }
+  # end
 
 end
