a5a707ef453f47d92ba3ee1538d62561e54c70c1
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-05 22:32:40 +0200

    exercise 7-8 started

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204080208) do
+ActiveRecord::Schema.define(version: 20160205203007) do
+
+  create_table "beerclubs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "founded"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
