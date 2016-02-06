0f787c2f3d239b68869dfbed46f18801be94aa4b
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-06 15:36:05 +0200

    exercise 10 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
-  validates :password, length: {minimum: 4}
+  validates :password, length: {minimum: 4},
+            format: {with: /\A[A-Z]+[\d]+\z/, message: "only allows capital letters and digits"}
   has_many :ratings
   has_many :beers, through: :ratings
   has_many :beerclubs
