3110721c0c52ab0d4cf55ddaa3c273adf8dd982d
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-13 23:18:01 +0200

    everything done until exercise 5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -59,22 +59,24 @@ RSpec.describe User, type: :model do
     end
 
     it "is the only rated if only one rating" do
-      beer = FactoryGirl.create(user, 10)
+      beer = create_beer_with_rating(user, 10)
       expect(user.favorite_beer).to eq(beer)
     end
 
     it "is the one with highest rating if several rated" do
-      create_beer_with_ratings(user, 10, 20, 15, 7, 9)
+      create_beers_with_ratings(user, 10, 20, 15, 7, 9)
       best = create_beer_with_rating(user, 25)
+
       expect(user.favorite_beer).to eq(best)
     end
-  end
 
+  end
 end
+
 
 def create_beers_with_ratings(user, *scores)
   scores.each do |score|
-    create_beer_with_rating(user, score)
+    create_beer_with_rating user, score
   end
 end
 
