8cdbe4d5539d3381a023d84b9baacbf4cd0a4c52
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-28 23:49:37 +0200

    week 6 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,30 +26,45 @@ class User < ActiveRecord::Base
   end
 
   def favorite_style
-    return nil if ratings.empty?
-
-    rated = ratings.map { |r| r.beer.style }.uniq
-    rated.sort_by { |style| -rating_of_style(style) }.first
+    favorite :style
   end
 
   def favorite_brewery
+    favorite :brewery
+  end
+
+  def favorite(category)
     return nil if ratings.empty?
 
-    rated = ratings.map { |r| r.beer.brewery }.uniq
-    rated.sort_by { |brewery| -rating_of_brewery(brewery) }.first
+    rated = ratings.map{ |r| r.beer.send(category) }.uniq
+    rated.sort_by { |item| -rating_of(category, item) }.first
   end
+
+  def rating_of(category, item)
+    ratings_of = ratings.select{ |r| r.beer.send(category)==item }
+    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
+  end
+
+#  def method_missing(method_name, *args, &block)
+#    if method_name =~ /^favorite_/
+#      category = method_name[9..-1].to_sym
+#      self.favorite category
+#    else
+#      return super
+#    end
+#  end
 
   private
 
-  def rating_of_style(style)
-    ratings_of = ratings.select { |r| r.beer.style==style }
-    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
-  end
+#  def rating_of_style(item)
+#    ratings_of = ratings.select { |r| r.beer.style==item }
+#    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
+#  end
 
-  def rating_of_brewery(brewery)
-    ratings_of = ratings.select { |r| r.beer.brewery==brewery }
-    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
-  end
+#  def rating_of_brewery(item)
+#    ratings_of = ratings.select { |r| r.beer.brewery==item }
+#    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
+#  end
 
   def self.top(n)
     # palauta listalta parhaat n kappaletta
@@ -57,5 +72,7 @@ class User < ActiveRecord::Base
     n = n-1
     User.all.sort_by { |b| (b.average_rating||0) }[0..n]
   end
+
+  
 
 end
