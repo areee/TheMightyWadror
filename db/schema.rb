5536c31e23faa663cdb6e283821a5748acf29923
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-28 15:39:32 +0200

    exercise 10-11 updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227211742) do
+ActiveRecord::Schema.define(version: 20160228132829) do
 
   create_table "beerclubs", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160227211742) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.boolean  "admin"
+    t.boolean  "banned"
   end
 
 end
