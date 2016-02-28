2914c598c205db5ccb31e55615c8b21eb5317b05
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-02-28 23:11:22 +0200

    exercises 12-14 done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    reset_session
     user = User.find_by username: params[:username]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
